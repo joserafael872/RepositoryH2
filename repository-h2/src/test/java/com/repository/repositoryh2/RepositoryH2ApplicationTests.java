c0f7e3730979aed9b259aa07f00a7fa79232b833
1613667 <1613667@22hw035077.SOAM.TCS.com>
2020-04-21 15:35:35 -0500

    git ignore

---
+++ repository-h2/src/test/java/com/repository/repositoryh2/RepositoryH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repository.repositoryh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
